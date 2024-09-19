a47b5662c723e7dc18094b3b88cf7d484cfdcbcd
Kagoji Faysal <faysal@live-ent.net>
2024-09-19 19:15:08 +0600

    Production build war file generate

--- src/main/java/com/kagoji/atfarestfulapis/SpringRestfulApIsApplication.java
+++ src/main/java/com/kagoji/atfarestfulapis/SpringRestfulApIsApplication.java
@@ -2,9 +2,10 @@ package com.kagoji.atfarestfulapis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringRestfulApIsApplication {
+public class SpringRestfulApIsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestfulApIsApplication.class, args);
